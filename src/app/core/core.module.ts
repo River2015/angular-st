8431113800bfbbd19117087071c652b90ba2383d
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2020-11-18 12:57:30 +0300

    core module added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {CoursesComponent} from './courses/courses.component';
-import {ProfileComponent} from './modules/profile/profile.component';
-import {BreadcrumbsComponent} from './modules/breadcrumbs/breadcrumbs.component';
-import {CoursesSearchComponent} from './courses/modules/courses-search/courses-search.component';
-import {CourseComponent} from './courses/modules/course/course.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {BreadcrumbsComponent} from './components/breadcrumbs/breadcrumbs.component';
+import {CoursesSearchComponent} from './courses/components/courses-search/courses-search.component';
+import {CourseComponent} from './courses/components/course/course.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/core/modules/course.ts
@@ -0,0 +1,7 @@
+export interface ICourse {
+  id: number;
+  title: string;
+  creation: string;
+  duration: string;
+  description: string;
+}
