bb4c1ef4679cc8129c272fb6eb627c8c5c094388
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2020-11-23 12:48:39 +0300

    components structure refactoring

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {CoursesComponent} from './courses/courses.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {CoursesComponent} from './components/courses/courses.component';
 import {ProfileComponent} from './components/profile/profile.component';
 import {BreadcrumbsComponent} from './components/breadcrumbs/breadcrumbs.component';
-import {CoursesSearchComponent} from './courses/components/courses-search/courses-search.component';
-import {CourseComponent} from './courses/components/course/course.component';
+import {CoursesSearchComponent} from './components/courses/components/courses-search/courses-search.component';
+import {CourseComponent} from './components/courses/components/course/course.component';
 
 @NgModule({
   declarations: [
