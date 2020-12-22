cb6cb887bdb99d870d211fa98d3c30e7679e4530
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2020-12-22 12:42:44 +0300

    directive conditions ixed

--- src/app/core/directives/course-border.directive.ts
+++ src/app/core/directives/course-border.directive.ts
@@ -1,26 +1,25 @@
 import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 
 
-
 @Directive({
   selector: '[appCourseBorder]',
 })
 export class CourseBorderDirective implements OnInit {
   @Input() creation: string;
 
-  currentDate;
-  differenceDate;
+  private currentDate;
+  private differenceDate;
+  private creationDate;
   constructor(private element: ElementRef) {
   }
   ngOnInit(): void {
     this.currentDate = new Date();
-    this.differenceDate = 	new Date().setDate(this.currentDate.getDate() - 14 ).toLocaleString();
+    this.creationDate = new Date(this.creation);
+    this.differenceDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 14);
 
-    if (
-      this.creation < this.currentDate.toLocaleString() && this.creation >=  this.differenceDate
-    ) {
+    if (this.creationDate < this.currentDate && this.creationDate >=  this.differenceDate) {
       this.element.nativeElement.style.border = '1px solid green';
-    } else if (this.creation > this.currentDate.toLocaleString() ) {
+    } else if (this.creationDate > this.currentDate ) {
       this.element.nativeElement.style.border = '1px solid blue';
     } else { return; }
   }
--- src/app/core/mocks.ts
+++ src/app/core/mocks.ts
@@ -13,7 +13,7 @@ export const COURSES: Array<ICourse> = [
     id: 2,
     title: '2 course',
     duration: 120,
-    creation: '12/20/2020',
+    creation: '10/22/2020',
     description: 'A component instance has a lifecycle that starts when Angular instantiates the component class and renders the component view along with its child views. The lifecycle continues with change detection, as Angular checks to see when',
     topRated: false
   },
@@ -21,7 +21,23 @@ export const COURSES: Array<ICourse> = [
     id: 3,
     title: '3 Video',
     duration: 50,
-    creation: '10/01/2021',
+    creation: '10/30/2021',
+    description: 'A component instance has a lifecycle that starts when Angular instantiates the component class and renders the component view along with its child views. The lifecycle continues with change detection, as Angular checks to see when',
+    topRated: true
+  },
+  {
+    id: 4,
+    title: '4 course',
+    duration: 100,
+    creation: '12/20/2020',
+    description: 'A component instance has a lifecycle that starts when Angular instantiates the component class and renders the component view along with its child views. The lifecycle continues with change detection, as Angular checks to see when',
+    topRated: false
+  },
+  {
+    id: 5,
+    title: '5 course',
+    duration: 120,
+    creation: '12/21/2020',
     description: 'A component instance has a lifecycle that starts when Angular instantiates the component class and renders the component view along with its child views. The lifecycle continues with change detection, as Angular checks to see when',
     topRated: true
   }
