eb5b5894e8dd916982c28a18c81b32ed42ebd192
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2021-01-30 15:47:54 +0300

    logic changed for auth guard

--- client/src/app/core/guards/auth.guard.ts
+++ client/src/app/core/guards/auth.guard.ts
@@ -1,30 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 
-import { Observable } from 'rxjs';
+import { Observable, of} from 'rxjs';
 import { AuthService } from '../services/auth.service';
-//import {map, switchMap} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  // isLogged$: Observable<boolean> = this.authService.isAuth();
   constructor(private router: Router, private authService: AuthService) {}
-
   canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot ): | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>  | boolean | UrlTree {
-    const homePageUrlTree = this.router.parseUrl('/login');
-    // this.isLogged$.pipe(
-    //   map((isLogged: boolean) => {
-    //     console.log(isLogged);
-    //     return isLogged ? true : homePageUrlTree;
-    //     }
-    //   ),
-    // );
-
-    if (this.authService.isAuth()) { return true; }
-
-    return this.authService.isAuth() ? true : homePageUrlTree;
+    return this.authService.isAuth().pipe(
+      map((resp) => {
+      if (resp) {
+        return true;
+      }
+      this.router.parseUrl('/login');
+      return false;
+    }), catchError((error) => {
+      this.router.navigate(['/login']);
+      return of(false);
+    }));
   }
 }
