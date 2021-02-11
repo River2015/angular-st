c32621f8cfb2ecc92815dccae2759ae76ec3ecef
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2021-02-11 15:24:06 +0300

    login form added

--- client/src/app/core/components/login/login.component.ts
+++ client/src/app/core/components/login/login.component.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {CoursesAppState} from '../../models/courses-state.model';
 import {Login} from './store/user.actions';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'study-login',
@@ -11,27 +12,33 @@ import {Login} from './store/user.actions';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-  name = '';
-  password = '';
   // token;
-  payload = { login: 'Morales', password: 'id'}
+  form: FormGroup;
+
   constructor(private authService: AuthService,
               private store: Store<CoursesAppState>,
-              private router: Router) { }
+              private router: Router,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
     // this.store.subscribe(state =>
     //   this.token = state.user.token
     // );
+    this.createForm();
     this.store.subscribe(state =>
       console.log(state)
   );
   }
   login(): any{
-    this.store.dispatch(new Login(this.payload));
+    this.store.dispatch(new Login(this.form.value));
    // this.authService.getUser();
     this.router.navigateByUrl('/courses');
-    // this.authService.loginUser('Morales', 'id');
-    // console.log(this.authService.getUser(), 'login successfully');
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      login: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
   }
 }
